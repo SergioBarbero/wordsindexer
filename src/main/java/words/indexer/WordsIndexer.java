e57765cc3b1c0891f378fe671d84bb37910f56d6
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2019-08-02 12:27:41 +0200

    Added documentation

--- src/main/java/words/indexer/WordsIndexer.java
+++ src/main/java/words/indexer/WordsIndexer.java
@@ -14,14 +14,27 @@ public class WordsIndexer {
         this.words = Arrays.stream(text.toLowerCase().split("\\W+")).collect(toList());
     }
 
+    /**
+     * Gets the map with the information of the letter and words
+     * @return map
+     */
     public Map<Character, Set<String>> getLettersMap() {
         return getCharactersSet().stream().collect(toMap(Function.identity(), this::getContainedWordsByLetter));
     }
 
+    /**
+     * Gets a character set in lower case from the List of words
+     * @return character set
+     */
     private Set<Character> getCharactersSet() {
         return words.stream().flatMap(word -> word.chars().mapToObj(character -> (char) Character.toLowerCase(character))).collect(toSet());
     }
 
+    /**
+     * Gets Set of words that contains such letter
+     * @param letter
+     * @return set of words that contains the letter
+     */
     private SortedSet<String> getContainedWordsByLetter(char letter) {
         return words.stream().filter(x -> x.contains(String.valueOf(letter).toLowerCase())).collect(Collectors.toCollection(TreeSet::new));
     }
